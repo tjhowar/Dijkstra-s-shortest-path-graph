91a91e3de9cf12b22773763898be06b4b47aa079
Travis Howard <61389527+tjhoward@users.noreply.github.com>
2021-03-07 16:13:03 -0700

    Update heap.h

--- heap.h
+++ heap.h
@@ -1,9 +1,9 @@
 #ifndef HEAP_H // prevents double declarations
 #define HEAP_H
-
 #include <iostream>
 #include "graph.h"
 using namespace std;
+
 
 //This struct will be used to implement the min-heap.
 // The min heap will keep track of the shortest distance computed for each node in the graph
@@ -12,7 +12,6 @@ struct Element{
     int d; //distance
     int p; //parent
 };
-
 
 //Initialize the graph elements/nodes
 void Initialize(Graph* graph, int s);
@@ -23,7 +22,7 @@ void BuildHeap(Graph* graph,Element* A, int n);
 //Implements the Min-Heapify algorithm which sorts elements in an array so that they satisfy the min heap property.
 void MinHeapify(Element* A, int i, int arraySize);
 
-//used to swap values
+//used to swap node values
 void swap(int *x, int *y);
 
 // gets parent node
